575126c0553f98eff368ea6177dafa9f970d2dad
Thabang Rammitlwa <thabang451@gmail.com>
2024-06-13 09:39:43 +0200

    BrowserRouter,Routes and Route path

--- Podcast app/src/App.jsx
+++ Podcast app/src/App.jsx
@@ -5,10 +5,18 @@ import Episode from './components/Episode';
 import Genre from './components/Genre';
 import AudioPlayer from './components/Audioplayer';
 
+function App(){
+  <BrowserRouter>
+  <Routes>
+    <Route path="/"element={<Home/>}/>
+<Route path="/seasons/:id" element={<Seasons/>}/>
+<Route path="/episode/:id" element={<Episode/>}/>
+<Route path="/genre/:id" element={<Genre/>}/>
+<Route path="/audioplayer" element={<AudioPlayer/>}/>
 
-
-
-
-
+  </Routes>
+  
+  </BrowserRouter>
+}
 
 export default App
