77fcfe7481740287c2578bd2e687b3395bc917c2
Thabang Rammitlwa <thabang451@gmail.com>
2024-06-13 09:33:24 +0200

    Importing

--- Podcast app/src/App.jsx
+++ Podcast app/src/App.jsx
@@ -1,3 +1,14 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './components/Home';
+import Seasons from './components/Seasons';
+import Episode from './components/Episode';
+import Genre from './components/Genre';
+import AudioPlayer from './components/Audioplayer';
+
+
+
+
+
+
 
 export default App
