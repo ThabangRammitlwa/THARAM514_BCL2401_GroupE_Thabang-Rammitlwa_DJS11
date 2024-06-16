eb0903d6eb93474e1dd19c1a599883c175322eb5
Thabang Rammitlwa <thabang451@gmail.com>
2024-06-16 12:12:38 +0200

    Update Episode.jsx

--- Podcast app/src/components/Episode.jsx
+++ Podcast app/src/components/Episode.jsx
@@ -1,27 +1,35 @@
-import {useEffect , useState} from 'react';
-import {useParams} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-const Episode=()=>{
-    const {episodeId}= useParams();
-    const [episode,setEpisode]=useState({});
+const Episode = () => {
+  const { episodeId } = useParams();
+  const [episode, setEpisode] = useState(null);
 
-    useEffect(()=>{
-        const fetchEpisode=async()=>{
-            const response=await fetch(`https://podcast-api.netlify.app/episode/${episode}`);
-                const data=await response.json();
-                setEpisode(data);
-        }
-        fetchEpisode();
-        },[episodeId]);
+  useEffect(() => {
+    const fetchEpisode = async () => {
+      const response = await fetch(`https://podcast-api.netlify.app`);
+      const data = await response.json();
+      setEpisode(data);
+    };
 
-        return(
-            <div>
-                <h1>{episode.title}</h1>
-                <p>{episode.description}</p>
-                <audio controls src={episode.audio} />
-                </div>
-        )
-        }
-        export default Episode;
+    fetchEpisode();
+  }, [episodeId]); 
+
+  return (
+    <div>
+      {episode ? (
+        <div>
+          <h2>{episode.title}</h2>
+          {}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default Episode;
+
 
      
